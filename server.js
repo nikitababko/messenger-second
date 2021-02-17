31d257830249321c4bfca1aaaaa4ddb9e6e6f9bc
Nikita Babko <niki-babko@yandex.ru>
2021-02-17 18:16:42 +0700

    Replace all.

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, (err) => {
-  if (err) {
-    throw Error(err);
-  }
-  console.log(`Server has started on port ${PORT}`);
-});
