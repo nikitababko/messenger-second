e5d637f9fd0fee20a8cb719da7b72dd74308c07c
Nikita Babko <niki-babko@yandex.ru>
2021-02-17 19:04:37 +0700

    Added mongoose connection.

--- server/server.js
+++ server/server.js
@@ -1,3 +1,24 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on('error', (err) => {
+  console.log('Mongoose connection ERROR: ' + err.message);
+});
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connected');
+});
+// Import modules
+require('./models/User');
+require('./models/Chatroom');
+require('./models/Message');
+
 const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
