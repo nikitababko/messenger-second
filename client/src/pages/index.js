b33a0253e52a75788d7a846e03e5ff6c707b0a6a
Nikita Babko <niki-babko@yandex.ru>
2021-03-07 22:38:25 +0700

    Added Chatroom provider.

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,2 +1,4 @@
 export { default as Auth } from './Auth';
+export { default as Dashboard } from './Dashboard';
 export { default as Home } from './Home';
+export { default as Chatroom } from './Chatroom';
