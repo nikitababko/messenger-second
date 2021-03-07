3e21f56c9d6f3cc8c0608455717d2263c37561df
Nikita Babko <niki-babko@yandex.ru>
2021-03-07 22:34:53 +0700

    Added axios req.

--- client/src/modules/LoginForm/index.js
+++ client/src/modules/LoginForm/index.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-import { Form, Input, Button, Checkbox } from 'antd';
+import { Form, Input, Button } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
-const LoginForm = () => {
-  const onFinish = (values) => {
-    console.log('Received values of form: ', values);
+import makeToast from 'Toaster';
+
+const LoginForm = (props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loginUser = (e) => {
+    e.preventDefault();
+
+    const loginData = {
+      email,
+      password,
+    };
+
+    axios.post('/user/login', loginData).then((res) => {
+      makeToast('success', res.data.message);
+      localStorage.setItem('token', res.data.token);
+      props.history.push('/dashboard');
+    });
   };
 
   return (
     <Form
+      onSubmit={loginUser}
       name="normal_login"
       className="login-form"
       initialValues={{
         remember: true,
       }}
-      onFinish={onFinish}
     >
       <Form.Item
         name="email"
@@ -29,6 +46,8 @@ const LoginForm = () => {
         <Input
           prefix={<UserOutlined className="site-form-item-icon" />}
           placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
         />
       </Form.Item>
 
@@ -45,20 +64,18 @@ const LoginForm = () => {
           prefix={<LockOutlined className="site-form-item-icon" />}
           type="password"
           placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
         />
       </Form.Item>
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <a className="login-form-forgot" href="">
-          Forgot password
-        </a>
-      </Form.Item>
 
       <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
+        <Button
+          onClick={loginUser}
+          type="primary"
+          htmlType="submit"
+          className="login-form-button"
+        >
           Log in
         </Button>
         Or <a href="">register now!</a>
