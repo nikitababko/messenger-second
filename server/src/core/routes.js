cd62121e973f81348cd821fecf2960fd842c3254
Nikita Babko <niki-babko@yandex.ru>
2021-03-07 22:30:47 +0700

    Added chatroom route.

--- server/src/core/routes.js
+++ server/src/core/routes.js
@@ -1,12 +1,18 @@
 const express = require('express');
+
 const { catchErrors } = require('../handlers/errorHandlers');
 const UserController = require('../controllers/UserController');
+const ChatroomController = require('../controllers/ChatroomController');
+const auth = require('../middlewares/auth');
 
 const createRouters = (app) => {
   app.use(express.json());
 
   app.post('/user/login', catchErrors(UserController.login));
   app.post('/user/register', catchErrors(UserController.register));
+
+  app.get('/chatroom', catchErrors(ChatroomController.getAllChatrooms));
+  app.post('/chatroom', catchErrors(ChatroomController.createChatroom));
 };
 
 module.exports = createRouters;
