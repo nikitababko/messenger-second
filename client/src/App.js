1a2aec1b39ee6e6d37080d912f2f95f55ba91efa
Nikita Babko <niki-babko@yandex.ru>
2021-03-07 22:37:21 +0700

    Added Routes to dashboard and chatroom.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { Auth, Home } from './pages';
+import { Auth, Home, Dashboard, Chatroom } from './pages';
 
 import './styles/index.scss';
 
@@ -12,6 +12,8 @@ const App = () => {
         <Switch>
           <Route path={['/login', '/register']} component={Auth} />
           <Route exact path="/" component={Home} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/chatroom/:id" component={Chatroom} />
         </Switch>
       </Router>
     </div>
