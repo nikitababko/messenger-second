d3cff389c44425fed0e7cc19a85aade0020ca470
Nikita Babko <niki-babko@yandex.ru>
2021-03-07 22:36:37 +0700

    Added rules.

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,60 +1,16 @@
-import React from 'react';
-import { Form, Input, Button, Col, Row } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import React, { useEffect } from 'react';
 
-const RegisterForm = () => {
-  const onFinish = (values) => {
-    console.log('Received values of form: ', values);
-  };
+const Home = (props) => {
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      props.history.push('/login');
+    } else {
+      props.history.push('/dashboard');
+    }
+  }, []);
 
-  return (
-    <Form
-      name="normal_login"
-      className="login-form"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name="chatroom"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Username!',
-          },
-        ]}
-      >
-        <Input
-          prefix={<UserOutlined className="site-form-item-icon" />}
-          placeholder="Chatroom"
-        />
-      </Form.Item>
-
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Create chatroom
-        </Button>
-      </Form.Item>
-
-      <Form.Item label="Happy newbie">
-        <Row gutter={8}>
-          <Col span={12}>
-            <Form.Item
-              name="captcha"
-              noStyle
-              rules={[{ required: true, message: 'Please input the captcha you got!' }]}
-            >
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Button>Join</Button>
-          </Col>
-        </Row>
-      </Form.Item>
-    </Form>
-  );
+  return <div>Home</div>;
 };
 
-export default RegisterForm;
+export default Home;
